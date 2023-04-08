1d7d133eb3b4dac82e1f1b753f44fb6a7e43b4f9
hong-sehyun <sese0733@naver.com>
2023-04-08 14:27:58 +0900

    매서드 호출

---
+++ src/datastructure/Code16.java
@@ -0,0 +1,28 @@
+package datastructure;
+
+import java.util.Scanner;
+
+public class Code16 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+
+		int result = power(a, b);
+
+		System.out.println(result);
+		sc.close();
+	}
+	
+	static int power( int n, int m )
+	{
+		int prod = 1;
+		for(int i = 0; i < m; i++) {
+			prod *= n;    // prod = prod * n
+		}
+		
+		return prod;
+	}
+}
