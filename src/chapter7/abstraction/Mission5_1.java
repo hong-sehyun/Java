35296c7d4ff9cf0ed9dbf83003b0557ec5ea9c3a
hong-sehyun <sese0733@naver.com>
2023-04-13 14:28:40 +0900

    Mission5 응용

---
+++ src/chapter7/abstraction/Mission5_1.java
@@ -0,0 +1,22 @@
+package chapter7.abstraction;
+
+import java.util.Scanner;
+
+public class Mission5_1 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int price = sc.nextInt();
+				
+
+		int[] arr = new int[0];
+		
+		for(int i = 0; i < 50000; i++) {
+			arr[i] = 
+		}
+	}
+	//미션5 문제를 배열을 푸는법???
+
+}
