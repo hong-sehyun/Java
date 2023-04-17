3fbd0898dad01855174fb3e4a92ef67da2d56992
hong-sehyun <sese0733@naver.com>
2023-04-17 13:10:33 +0900

    기본 API 활용

--- src/chapter9/Test02.java
+++ src/chapter9/Test02.java
@@ -1,5 +1,7 @@
 package chapter9;
 
+import java.util.Arrays;
+
 public class Test02 {
 
 	public static void main(String[] args) {
@@ -9,7 +11,7 @@ public class Test02 {
 		String s3 = new String("Java");
 		String s4 = "Java";
 		
-		if(s1 == s3) {
+		if(s1.equals(s3)) {
 			System.out.println("동일 객체이다.");
 		} else {
 			System.out.println("다른 객체이다.");
@@ -17,7 +19,8 @@ public class Test02 {
 		
 		
 		System.out.println("s1 hashCode : " + s1.hashCode());
-		System.out.println("s3 hashCode : " + s3.hashCode());
+		System.out.println("s3 hashCode : " + s3.hashCode()); //hashCode 가 같은 이유: 코드 최적화
+		
 		System.out.println("s1 identityHashCode : " + System.identityHashCode(s1));
 		System.out.println("s3 identityHashCode : " + System.identityHashCode(s3));
 
@@ -27,13 +30,17 @@ public class Test02 {
 
 
 		
-		if(s2 == s4) {
+		if(s1 == s3) {
 			System.out.println("동일 객체이다.");
 		} else {
 			System.out.println("다른 객체이다.");
 		}
 		
 
+		int i3 = Integer.parseInt("123");
+		
+		Arrays.binarySearch(null, 0);
+		Arrays.sort(null);
 	}
 
 }
