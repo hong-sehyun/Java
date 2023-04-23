6bd311e3feb15ab006bda259edac232b36e9fbbe
hongsehyun <hongsehyun@218.38.137.28>
2023-04-23 17:17:54 +0900

    메서드 호

---
+++ src/datastructure/Code18.java
@@ -0,0 +1,24 @@
+package datastructure;
+
+public class Code17 {
+
+	public static void main(String[] args) {
+
+		for (int n = 2; n < 100000; n++) {
+			if (isPrime(n)) {
+				System.out.println(n);
+			}
+		}
+
+	}
+
+	static boolean isPrime(int k) {
+		for (int i = 2; i * i <= k; i++)
+			if (k % i == 0)
+				return false;
+
+		return true;
+
+	}
+
+}
